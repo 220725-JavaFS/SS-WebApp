0257f15eca2acb191640a7a55a3c9c417454de22
BlizWoRo <saumik148@revature.net>
2022-08-17 19:47:25 -0500

    WebApp Start Skeleton v2

---
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -0,0 +1,57 @@
+package com.revature.utils;
+
+import java.sql.Connection; //java.sql is from JDBC package
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+
+	//A singleton design pattern only allows one instance of a Class to exist in memory at a time.
+	//connection here is a singleton
+	
+	private static Connection connection;
+	
+	public static Connection getConnection() throws SQLException{
+		
+		if(connection != null && !connection.isClosed()) {
+			
+			return connection;
+		
+		}else {
+			
+			//For many frameworks or in cases where there are multiple SQL drivers you will need to register which driver you are using for the connection interface.
+			//The Class.forName method will allow you to do this. This step is often unnecessary for simple projects.
+			
+			try {
+				
+				Class.forName("org.postgresql.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			String url = "jdbc:postgresql://blizworojavafs072522.cqefx7g9mnjf.us-east-2.rds.amazonaws.com:5432/demos";
+			String username = "postgres"; //It is possible to hide raw credentials using ENV variables.
+			String password = "JavaPassWord01"; //You can access those variables using System.getenv("var-name");
+			
+			connection = DriverManager.getConnection(url, username, password);
+			
+			return connection;
+			
+		}
+		
+	}
+	
+	public static void main (String[] args) {
+		
+		try {
+			getConnection();
+			System.out.println("Connection Successful!");
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
